50c3bc97cdd552789e90367f8716a5471ba5dd15
Berly <Berly@DESKTOP-M45AQK7>
2024-05-30 15:09:59 +0300

    add global exception handler and serilog

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,9 +1,14 @@
+using API.ExceptionHandler;
 using Application;
 using Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 IConfiguration configuration = builder.Configuration;
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services
     .AddApplication()
@@ -11,10 +16,17 @@ builder.Services
 
 builder.Services.AddControllers();
 
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseExceptionHandler();
+app.UseRouting();
+app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
